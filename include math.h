c2c91a6eea5db8636122f91ff56ea8e9a8448904
faseel77 <faseelmhd9@gmail.com>
2024-10-07 12:02:16 +0530

    array

---
+++ include math.h
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <math.h>
+int main()
+{ int x = 25;
+ printf("%f\n",sqrt(x) );
+ printf("%f\n",ceil(4.7));
+ printf("%f\n", pow(2,2));
+ printf("%f",floor(5.0));  
+ 
+}
+
